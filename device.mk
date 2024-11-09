e8af13c7a3821d2c587846a7e8db7f0cec24fb6e
clarencelol <clarencekuiek@proton.me>
2024-11-10 00:37:51 +0530

    hanoip: Switch to the pixel power-libperfmgr > Drop lineage/sepolicy/libperfmgr as well and address it in DT instead > Satisfy FCM while at it
    Signed-off-by: Sapan Gajjar <sapangajjar101105@gmail.com>

--- device.mk
+++ device.mk
@@ -423,7 +423,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.hanoip-libperfmgr
+    android.hardware.power-service.pixel-libperfmgr
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
